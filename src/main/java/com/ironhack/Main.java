45aca7e128b5ea5b272d5b35230b0487f279c20f
Joel Campos <joelcamposoliva9000@gmail.com>
2022-07-19 19:27:55 +0200

    Main cleanup

--- src/main/java/com/ironhack/Main.java
+++ src/main/java/com/ironhack/Main.java
@@ -1,23 +1,7 @@
 package com.ironhack;
 
-import com.ironhack.commander.Command;
-import com.ironhack.commander.Commander;
-
-enum CommandType {
-    saySth
-}
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        var commander = new Commander<CommandType>(new Command[] {
-            new Command<>("say :word", CommandType.saySth).addOnRun((cr) -> {
-                System.out.println(cr.getParameter("word"));
-            })
-        });
-
-        final var result = commander.askForCommand();
-        result.run();
     }
 }
