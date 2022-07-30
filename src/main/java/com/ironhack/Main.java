eba17e8bd7ab5dd812e2aabd720a81f50492c919
sjacas <sergi.jacas@wezaam.com>
2022-07-30 17:32:57 +0200

    Reset main

--- src/main/java/com/ironhack/Main.java
+++ src/main/java/com/ironhack/Main.java
@@ -1,15 +1,7 @@
 package com.ironhack;
 
-import com.ironhack.serialization.Serialization;
-import com.ironhack.serialization.Student;
-import com.ironhack.serialization.Teacher;
-
-import java.util.Map;
-
-import static com.ironhack.serialization.Table.*;
 
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
