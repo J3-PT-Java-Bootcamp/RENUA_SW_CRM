4a27f5c615fd3379331f86148b53a6c17e763b0f
sjacas <sergi.jacas@wezaam.com>
2022-07-30 17:31:53 +0200

    Remove table enum

--- src/main/java/com/ironhack/serialization/Serialization.java
+++ src/main/java/com/ironhack/serialization/Serialization.java
@@ -1,36 +1,24 @@
 package com.ironhack.serialization;
 
 import java.io.*;
-import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Serialization<T> {
 
     // Creating a map of the file names and the file paths.
-    private final static EnumMap<Table, String> TABLES;
+    private final static String DATABASE = "src/main/java/com/ironhack/serialization/serialised-data.txt";
 
-
-    // A static block. It is executed when the class is loaded, before the constructor.
-    static {
-        TABLES = new EnumMap<>(Table.class);
-        TABLES.put(Table.USER, "src/main/java/com/ironhack/serialization/serialised-data-user.txt");
-        TABLES.put(Table.ACCOUNT, "src/main/java/com/ironhack/serialization/serialised-data-account.txt");
-        TABLES.put(Table.LEAD, "src/main/java/com/ironhack/serialization/serialised-data-lead.txt");
-        TABLES.put(Table.OPPORTUNITY, "src/main/java/com/ironhack/serialization/serialised-data-opportunity.txt");
-        TABLES.put(Table.CONTACT, "src/main/java/com/ironhack/serialization/serialised-data-contact.txt");
-    }
 
     /**
      * It takes a map and a file name, and saves the map to the file using Serialization.
      *
      * @param map The map that you want to save.
-     * @param table The name of the file to be saved.
      */
-    private static void save(Map map, Table table) {
+    private static void save(Map map) {
 
         try {
-            FileOutputStream fos = new FileOutputStream(TABLES.get(table));
+            FileOutputStream fos = new FileOutputStream(DATABASE);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(map);
         } catch (FileNotFoundException e) {
@@ -45,14 +33,13 @@ public class Serialization<T> {
     /**
      * It reads the file and converts the bytes to an object
      *
-     * @param table The name of the file to be read.
      * @return A map of the data that was saved in the file.
      */
-    private static Map read(Table table) throws FileNotFoundException {
+    private static Map read() throws FileNotFoundException {
         Map map = null;
 
         try {
-            FileInputStream fis = new FileInputStream(TABLES.get(table));
+            FileInputStream fis = new FileInputStream(DATABASE);
             ObjectInputStream ois = new ObjectInputStream(fis);
             map = (Map) ois.readObject();
         } catch (FileNotFoundException e) {
@@ -72,14 +59,13 @@ public class Serialization<T> {
      * Read the file, get the object with the given id, and return it.
      *
      * @param id The id of the object you want to get.
-     * @param table The name of the file to read from.
      * @return The object with the given id.
      */
-    public static Serialize getById(int id, Table table) {
+    public static Serialize getById(int id) {
         Map map;
 
         try {
-            map = read(table);
+            map = read();
             return (Serialize) map.get(id);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -90,14 +76,13 @@ public class Serialization<T> {
     /**
      * Reads the file and returns a map of all the key-value pairs
      *
-     * @param table The name of the file to read from.
      * @return A map of the contents of the file.
      */
-    public static Map getAll(Table table) {
+    public static Map getAll() {
         Map map;
 
         try {
-            map = read(table);
+            map = read();
             return map;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -110,14 +95,13 @@ public class Serialization<T> {
      * map, add the object to the map, and save the map
      *
      * @param object The object to be saved.
-     * @param table The name of the file to be saved.
      */
-    public static void put(Serialize object, Table table) {
+    public static void put(Serialize object) {
 
         Map map;
 
         try {
-            map = read(table);
+            map = read();
         } catch (FileNotFoundException e) {
             map = new HashMap();
         }
@@ -127,63 +111,60 @@ public class Serialization<T> {
         }
 
         map.put(object.getId(), object);
-        save(map, table);
+        save(map);
     }
 
     /**
      * Replace the object in the file with the same id as the object passed in
      *
      * @param object the object to be replaced
-     * @param table The name of the file to be read.
      */
-    public static void replace(Serialize object, Table table) {
+    public static void replace(Serialize object) {
         Map map;
 
         try {
-            map = read(table);
+            map = read();
         } catch (FileNotFoundException e) {
             map = new HashMap();
         }
 
         map.replace(object.getId(), object);
-        save(map, table);
+        save(map);
     }
 
     /**
      * Delete the object from the map.
      *
      * @param object The object to be deleted.
-     * @param table The name of the file to be saved.
      */
-    public static void delete(Serialize object, Table table) {
+    public static void delete(Serialize object) {
         Map map;
 
         try {
-            map = read(table);
+            map = read();
         } catch (FileNotFoundException e) {
             map = new HashMap();
         }
 
         map.remove(object.getId());
-        save(map, table);
+        save(map);
     }
 
     /**
      * Delete the record with the given id from the given table.
      *
      * @param id The id of the object you want to delete.
-     * @param table The table to save to.
      */
-    public static void delete(int id, Table table) {
+    public static void delete(int id) {
         Map map;
 
         try {
-            map = read(table);
+            map = read();
         } catch (FileNotFoundException e) {
             map = new HashMap();
         }
 
         map.remove(id);
-        save(map, table);
+        save(map);
     }
 }
--- src/main/java/com/ironhack/serialization/Table.java
+++
@@ -1,9 +0,0 @@
-package com.ironhack.serialization;
-
-public enum Table {
-    USER,
-    ACCOUNT,
-    LEAD,
-    OPPORTUNITY,
-    CONTACT
-}
