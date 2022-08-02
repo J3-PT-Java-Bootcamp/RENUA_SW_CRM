b1e427e86b983235d57f2ec985930fed747ac521
Joel Campos <joelcamposoliva9000@gmail.com>
2022-08-02 18:34:21 +0200

    Basic command

--- src/main/java/com/ironhack/Main.java
+++ src/main/java/com/ironhack/Main.java
@@ -1,7 +1,25 @@
 package com.ironhack;
 
+import com.ironhack.commander.*;
+
+enum CommandTypes {
+    NEW_LEAD,
+    EXIT
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final var commander = new Commander<CommandTypes>(new Command[] {
+                new Command<>("exit", CommandTypes.EXIT),
+            new Command<>("new lead", CommandTypes.NEW_LEAD),
+        });
+
+        // Run event when a command is executed
+        commander.setAutorun(true);
+
+        do {
+            var command = commander.askForCommand();
+            if(command.getResult() == CommandTypes.EXIT) break;
+        } while (true);
     }
 }
---
+++ src/main/java/com/ironhack/service/LeadService.java
@@ -0,0 +1,5 @@
+package com.ironhack.service;
+
+public class LeadService {
+
+}
